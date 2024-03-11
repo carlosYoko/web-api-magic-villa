92a46b755dcf3a56c46961807f7e5e16eea3237b
CGimenez <carlosgibe@gmail.com>
2024-03-11 21:27:26 +0100

    added docker-compose file

--- MagicVilla_API/Models/Villa.cs
+++ MagicVilla_API/Models/Villa.cs
@@ -5,6 +5,8 @@ namespace MagicVilla_Api.Models
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+
+        public int MyProperty { get; set; }
         public DateTime DateCreation { get; set; }
     }
 }
