e8d3ff272d5473b366d73cd906f2e89add75ccd4
CGimenez <carlosgibe@gmail.com>
2024-03-11 21:37:15 +0100

    added properties on Villa model and VillaDto model

--- MagicVilla_API/Models/Dto/VillaDto.cs
+++ MagicVilla_API/Models/Dto/VillaDto.cs
@@ -9,7 +9,18 @@ namespace MagicVilla_Api.Models.Dto
         [Required]
         [MaxLength(30)]
         public string? Name { get; set; }
+
+        public string? Detail { get; set; }
+
+        [Required]
+        public double Fee { get; set; }
+
         public int Occupants { get; set; }
+
         public int SquareMeters { get; set; }
+
+        public string? UrlImage { get; set; }
+
+        public string? Amenity { get; set; }
     }
 }
--- MagicVilla_API/Models/Villa.cs
+++ MagicVilla_API/Models/Villa.cs
@@ -1,12 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MagicVilla_Api.Models
 
 {
     public class Villa
     {
+        [Key]
         public int Id { get; set; }
+
         public string? Name { get; set; }
 
-        public int MyProperty { get; set; }
+        public string? Detail { get; set; }
+
+        [Required]
+        public double Fee { get; set; }
+
+        public int Occupants { get; set; }
+
+        public int SquareMeters { get; set; }
+
+        public string? UrlImage { get; set; }
+
+        public string? Amenity { get; set; }
+
         public DateTime DateCreation { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+
     }
 }
