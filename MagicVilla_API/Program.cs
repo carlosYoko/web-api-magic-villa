44bfd30f8be5d9213df3ff4bdb0c926e0b22bdb7
CGimenez <carlosgibe@gmail.com>
2024-03-11 15:21:40 +0100

    instaled and configured NewtonsoftJson

--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,11 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers(); // Agregar soporte para controladores
 
 var app = builder.Build();
 
